053d17cc280f73307f1a5a3fbeb9dac4caa86932
Der-Feb <bugingoderfeb@gmail.com>
2026-04-24 13:40:38 +0200

    singleton functionality

--- src/main/java/java/learn/singleton/TestSignleton.java
+++ src/main/java/java/learn/singleton/TestSignleton.java
@@ -5,4 +5,10 @@ public class TestSignleton {
     public static void main(String[] args) {
         Browser.getInstance().display();
     }
+
+
+    /* 
+        add enums, and also threads to work on the browser, and make browser robust 
+        and then explain the single ton and what you did in the explain.md
+    */ 
 }
